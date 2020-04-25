2b0de4e394cc62f87d8512c1265372ea43fd100d
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-04-25 01:47:59 +0100

    ecommerce design setup complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './App.css';
-import HomePage from "./homepage.components";
+import HomePage from "./pages/hompage/homepage.components";
 
 function App() {
   return (
