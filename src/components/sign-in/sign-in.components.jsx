ec326d9ae9085329ef8d58c84a509de57749451c
Jonathan-Odia Osahon Emmanuel <emmyodia@gmail.com>
2020-05-04 01:47:40 +0100

    edits

--- src/components/sign-in/sign-in.components.jsx
+++ src/components/sign-in/sign-in.components.jsx
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.components';
-import { signInWithGoogle } from  '../../firebase/firebase.utils.js';
+import { auth,signInWithGoogle } from  '../../firebase/firebase.utils.js';
+
 
 class SignIn extends Component{
     constructor(props){
@@ -14,9 +15,18 @@ class SignIn extends Component{
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
         
+        const{email,password} = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({email:'',password:''});
+        }catch(error){
+            console.log(error); 
+        }
+
         this.setState({email:'', password:''})
     }
 
--- src/pages/sign-in-up/Sign-in-up.components.jsx
+++ src/pages/sign-in-up/Sign-in-up.components.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './sign-in-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.components';
+import SignUp from '../../components/sign-up/sign-up.components';
 
 
 const SignInOut = () => {
     return(
-        <div className='signUpandIn'> <SignIn/></div>
+        <div className='signUpandIn'> 
+            <SignIn/>
+            <SignUp/>
+        </div>
        
     )
 }
